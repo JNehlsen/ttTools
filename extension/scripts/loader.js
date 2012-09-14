94a750cee155d81a82268afc96d390fe6775190a
Jack Nehlsen <jack.nehlsen@gmail.com>
2012-09-14 11:19:26 -0500

    updated loader

--- extension/scripts/loader.js
+++ extension/scripts/loader.js
@@ -1,4 +1,4 @@
 var script  = document.createElement('script');
 script.type = 'text/javascript';
-script.src  = 'https://raw.github.com/JNehlsen/ttTools/master/releases/latest/ttTools.min.js';
+script.src  = 'https://raw.github.com/JNehlsen/ttTools/master/releases/latest/ttTools.extension.min.js';
 document.head.appendChild(script);
